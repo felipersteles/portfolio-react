eaef1f65347e66c17bc163f2284b92cf4404da7b
Felipe Teles <55213753+felipersteles@users.noreply.github.com>
2022-11-11 11:07:04 -0300

    Update BlogData.js

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -1,6 +1,14 @@
 export const Blogs = [
     {
         id:1,
+        name:"Seja Full-Stack com REACT, Router 6, .NET 6 + EF Core - 2022",
+        tags:[".NET","C#","React","JavaScript","Swagger"],
+        date:"10 Novembro, 2022",
+        imgSrc:"https://img-b.udemycdn.com/course/240x135/3555839_1a8b_4.jpg",
+        link:"https://www.udemy.com/certificate/UC-bee222de-7033-4981-987f-2d51dbcd73b9/"
+    },
+    {
+        id:2,
         name:"Deep Learning Prático com TensorFlow e Python",
         tags:["deep learning","tensorflow","python"],
         date:"29 Março, 2021",
@@ -8,7 +16,7 @@ export const Blogs = [
         link:"https://www.udemy.com/certificate/UC-0ed23f84-48fc-4d16-b07c-3e9dd9bb8d5a/"
     },
     {
-        id:2,
+        id:3,
         name:"Curso Web Moderno Completo com JavaScript 2022 + Projetos",
         tags:["html","css","javascript", "jQuery", "Ajax"],
         date:"1 Junho, 2021",
@@ -16,7 +24,7 @@ export const Blogs = [
         link:"https://www.udemy.com/certificate/UC-7d0312fa-48b4-480b-a60a-07d18888e6e4/"
     },
     {
-        id:3,
+        id:4,
         name:"Linux Ubuntu e Libre Office",
         tags:["linux","ubuntu","Libre Office"],
         date:"26 Outubro, 2020",
